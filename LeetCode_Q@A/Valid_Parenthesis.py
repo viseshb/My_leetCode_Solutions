497f8ee8162316b95f119f83b755dd542c5959c6
viseshb <visesh66@gmail.com>
2025-04-07 22:33:47 -0500

    add new file

---
+++ LeetCode_Q@A/Valid_Parenthesis.py
@@ -0,0 +1,46 @@
+#20. Valid Parentheses
+#https://leetcode.com/problems/valid-parentheses/description/?envType=problem-list-v2&envId=stack
+
+class Solution:
+
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+
+    def size(self):
+        return len(self.stack_list)
+
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list.pop()
+
+    def isValid(self, s: str) -> bool:
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+
+        for char in s:
+            if char in bracket_map.values():  # opening brackets
+                stack.append(char)
+            elif char in bracket_map:  # closing brackets
+                if not stack or stack[-1] != bracket_map[char]:
+                   return False
+                stack.pop()
+
+        return not stack
