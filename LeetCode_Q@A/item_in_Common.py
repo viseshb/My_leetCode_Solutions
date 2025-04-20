32bb65d94f13f5ac98ede4a6a28d09ada0169a04
viseshb <visesh66@gmail.com>
2025-04-19 22:24:04 -0500

    add new file

--- LeetCode_Q@A/item_in_Common.py
+++ LeetCode_Q@A/item_in_Common.py
@@ -1,14 +1,20 @@
+#HT: Item In Common ( ** Interview Question)
+"""Write a function item_in_common(list1, list2) that takes two lists as input and returns True if 
+there is at least one common item between the two lists, False otherwise.
+Use a dictionary to solve the problem that creates an O(n) time complexity."""
+
 def item_in_common(list1, list2):
     my_dict = {}
     for i in list1:
-        my_dict[i] = True
+        my_dict[i] = 0
 
     for j in list2:
         if j in my_dict:
-           return True 
+            return True
+    return False
 
 
-list1 = [1,3,5]
-list2 = [2,4,5]
+list1 = [1,2,3]
+list2 = [4,5,3]
 
-print(item_in_common(list1, list2))
+print(item_in_common(list1,list2))
